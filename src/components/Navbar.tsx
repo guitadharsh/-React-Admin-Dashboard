2cccf8eb6255cde5fa6abd071a3d615c973c49e1
adharsh <adharshd100@gmail.com>
2024-03-06 01:28:19 +0530

    background animation logic rewamped

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar: React.FC = () => {
     return (
-        <div>Navbar</div>
+        <div className='navbar'>Navbar</div>
     )
 }
 
--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,22 +1,31 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import topGradient from '../assets/top.svg'
-import bottomGradient from '../assets/bottom.svg'
+// import topGradient from '../assets/top.svg'
+// import bottomGradient from '../assets/bottom.svg'
+import { Navbar, Sidebar } from '../components'
 
 const AdminLayout: React.FC = () => {
     return (
-        <div className='admin'>
-            <div className="admin__bg--top">
-                <img src={topGradient} alt="gradient-background" />
+        <>
+            <div className='admin'>
+                <div className="admin__bg"></div>
+
+                <Navbar />
+                <Sidebar />
+                <Outlet />
             </div>
 
-            <div className="admin__bg--bottom">
-                <img src={bottomGradient} alt="gradient-background" />
-            </div>
 
-            <Outlet />
-        </div>
+        </>
     )
 }
 
 export default AdminLayout
+
+{/* <div className="admin__bg--top">
+                    <img src={topGradient} alt="gradient-background" />
+                </div>
+
+                <div className="admin__bg--bottom">
+                    <img src={bottomGradient} alt="gradient-background" />
+                </div> */}
