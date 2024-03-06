c489e786efe17511323d4dd07a633d7bf8f72668
adharsh <adharshd100@gmail.com>
2024-03-06 22:50:15 +0530

    table page partially completed

--- src/components/EventTable.tsx
+++ src/components/EventTable.tsx
@@ -1,9 +1,63 @@
 import React from 'react'
+import { tableHeading, tableData } from '../data/tableData'
+import eye from '../assets/eye.svg'
 
-const Table: React.FC = () => {
+const EventTable: React.FC = () => {
   return (
-    <div>Table</div>
+    <div className='eventtable'>
+      <table>
+        <thead>
+          <tr>
+            {
+              tableHeading?.map((item, index) => {
+                return (
+                  <>
+                    <th key={index}>
+                      <p>
+                        {item?.title}
+                      </p>
+                    </th>
+                  </>
+                )
+              })
+            }
+
+          </tr>
+        </thead>
+
+        <tbody>
+          {
+            tableData?.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td><img src={eye} alt="eye-icon" /></td>
+                  <td>{item?.name}</td>
+                  <td>{item?.start}</td>
+                  <td>{item?.end}</td>
+                  <td>{item?.client}</td>
+                  <td>{item?.contact}</td>
+                  <td>{item?.venue}</td>
+                  <td>{item?.city}</td>
+                  <td>{item?.state}</td>
+                  <td>{item?.zipcode}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+
+      <div className="eventtable__footer">
+        <div className="eventtable__footer--nums">
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          <p className='active'>4</p>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
-export default Table
+export default EventTable
---
+++ src/data/tableData.ts
@@ -0,0 +1,43 @@
+const tableHeading = [
+    { title: '', isSort: true },
+    { title: 'Event Name', isSort: false },
+    { title: 'Event Start', isSort: false },
+    { title: 'Event End', isSort: true },
+    { title: 'Client Name', isSort: true },
+    { title: 'Contact Info', isSort: false },
+    { title: 'Venue', isSort: false },
+    { title: 'City', isSort: false },
+    { title: 'State', isSort: false },
+    { title: 'Zip Code', isSort: false },
+]
+
+const tableData = [
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+    { name: 'Filled Name', start: 'Jan 12, 2024', end: 'Jan 12, 2024', client: 'Mohammad Asad', contact: '+12 923829129', venue: 'St street Adress 123', city: 'hyrdrabad',  state: 'Telungana', zipcode: '695534'},
+]
+
+
+export { tableHeading, tableData }
