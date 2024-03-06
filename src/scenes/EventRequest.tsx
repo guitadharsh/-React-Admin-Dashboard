ba19013fb0f813af05653cc4e47757216700165a
adharsh <adharshd100@gmail.com>
2024-03-06 16:07:39 +0530

    evenrequest page layout setup

--- src/scenes/EventRequest.tsx
+++ src/scenes/EventRequest.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import plusImg from '../assets/plus.svg'
+import searchImg from '../assets/search.svg'
 
 const EventRequest: React.FC = () => {
   return (
-    <div className='eventrequest'>EventRequest
-      <h1>hello</h1>
+    <div className='eventreq'>
+      <div className="eventreq__heading">
+        <h1>Event Requests</h1>
+
+        <div className="eventreq__search">
+
+          <div className="eventreq__input">
+            <img src={searchImg} alt="search-icon" className="search-icon" />
+            <input type="text" placeholder='Search here' />
+          </div>
+
+          <button>
+            <img src={plusImg} alt="plus-icon" />
+          </button>
+          
+        </div>
+      </div>
     </div>
   )
 }
